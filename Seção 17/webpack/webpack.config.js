4c9281a07a518b0aae5e6b454699dffef53a9422
CappiJonas <cappijonas@gmail.com>
2021-01-11 18:13:57 -0300

    Começando o estudo sobre jQuery

--- Seção 17/webpack/webpack.config.js
+++ Seção 17/webpack/webpack.config.js
@@ -1,12 +1,28 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizaCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizaCSSAssetsPlugin({})
+        ] 
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -15,12 +31,16 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.css$/, 
+            test: /\.s?[ac]ss$/, 
             use: [
                 MiniCssExtractPlugin.loader,
                 // 'style-loader', //Adiciona CSS a DOM injetando a tag <style>
-                'css-loader' // interpreta @import, url()...
+                'css-loader', // interpreta @import, url()...
+                'sass-loader'
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
